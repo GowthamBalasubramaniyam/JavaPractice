51a2222187212779ff842975a107d3dc48f47c19
GowthamBalasubramaniyam <gowthambalasubramaniyam1@gmail.com>
2025-09-12 11:11:18 +0530

    Arrays

--- src/example/arrays/ArrayExample.java
+++ src/example/arrays/ArrayExample.java
@@ -4,16 +4,20 @@ public class ArrayExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//filling arrays using loops
 		int[] marks = new int[6];
 		int val=95;
 		for(int i = 0; i<marks.length;i++,val++) {
 			marks[i]=val;
 		}
 		
+		//loop through array
 		for(int j=0;j<marks.length;j++) {
 			System.out.println(marks[j]);
 		}
 		
+		//finding maximum 
 		int max = marks[0];
 		for(int k = 0;k<marks.length;k++) {
 			if(marks[k]>max) {
