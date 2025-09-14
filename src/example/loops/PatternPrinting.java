d6879cd456232dfc17f279870531aece2b253812
GowthamBalasubramaniyam <gowthambalasubramaniyam1@gmail.com>
2025-09-14 16:42:56 +0530

    Pattern printing completed

--- src/example/loops/PatternPrinting.java
+++ src/example/loops/PatternPrinting.java
@@ -6,23 +6,23 @@ public class PatternPrinting {
 		// pattern printing using for loop
 		// square pattern
 		int z = 5;
-
+		System.out.println("Square pattern");
 		for (int m = 1; m <= z; m++) {
 			for (int n = 1; n <= z; n++) {
 				System.out.print("* ");
 			}
 			System.out.println();
 		}
-
 		// increasing triangle
+		System.out.println("Left Increasing triangle");
 		for (int x = 1; x <= z; x++) {
 			for (int y = 1; y <= x; y++) {
 				System.out.print("* ");
 			}
 			System.out.println();
 		}
-		System.out.println("----------");
 		// decreasing triangle
+		System.out.println("Left Decreasing triangle");
 		for (int a = 1; a <= z; a++) {
 			for (int b = a; b <= z; b++) {
 				System.out.print("* ");
@@ -30,9 +30,9 @@ public class PatternPrinting {
 			System.out.println();
 		}
 
-		System.out.println("-----------");
-
 		// right sided triangle
+		System.out.println("Right increasing triangle");
+
 		for (int o = 1; o <= z; o++) {
 			for (int p = o; p <= z; p++) {
 				System.out.print("  ");
@@ -42,8 +42,47 @@ public class PatternPrinting {
 			}
 			System.out.println();
 		}
-		
-		
+		//hill pattern
+		System.out.println("Hill pattern");
+		for(int i = 1;i<z;i++) {
+			for(int j=i;j<=z;j++) {
+				System.out.print("  ");
+			}
+			for(int j=1;j<i;j++) {
+				System.out.print("* ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		//Diamond pattern
+		System.out.println("Diamond pattern");
+		for(int i = 1;i<z;i++) {
+			for(int j=i;j<=z;j++) {
+				System.out.print("  ");
+			}
+			for(int j=1;j<i;j++) {
+				System.out.print("* ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for(int i = 1;i<=z;i++) {
+			for(int j =1;j<=i;j++) {
+				System.out.print("  ");
+			}
+			for(int j =i;j<z;j++) {
+				System.out.print("* ");
+			}
+			for(int j =i;j<=z;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println("----------");
 
 	}
 
